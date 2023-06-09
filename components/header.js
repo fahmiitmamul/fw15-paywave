4eb766b5bb5ecf7931b72356b406b24aa7ce7e95
fahmipraga <itmamul2004@gmail.com>
2023-06-09 17:20:02 +0700

    feat: add notification button

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { FiBell } from 'react-icons/fi'
+import { FiArrowDown, FiArrowUp, FiBell } from 'react-icons/fi'
 import Picture from '../public/picture.jpg'
 
 export default function Header() {
@@ -20,8 +20,67 @@ export default function Header() {
               <div className="text-gray-600">+62 8139 3877 7946</div>
             </div>
           </div>
-          <div>
-            <FiBell size={25} />
+          <div className="z-10">
+            <div className="dropdown dropdown-end">
+              <label
+                tabIndex={0}
+                className="flex items-center justify-center cursor-pointer"
+              >
+                <FiBell size={25} />
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-[500px] h-[500px]"
+              >
+                <div className="flex flex-col gap-5 overflow-scroll">
+                  <div className="flex gap-3 items-center p-5 rounded-lg shadow-lg border-2 border-gray-200">
+                    <FiArrowDown size={25} color="green" />
+                    <div>
+                      <div className="text-gray-400 text-lg">
+                        Accept from Joshua Lee
+                      </div>
+                      <div className="font-bold text-xl">Rp220.000</div>
+                    </div>
+                  </div>
+                  <div className="flex gap-3 items-center p-5 rounded-lg shadow-lg border-2 border-gray-200">
+                    <FiArrowUp size={25} color="red" />
+                    <div>
+                      <div className="text-gray-400 text-lg">
+                        Transfer to Deni
+                      </div>
+                      <div className="font-bold text-xl">Rp220.000</div>
+                    </div>
+                  </div>
+                  <div className="flex gap-3 items-center p-5 rounded-lg shadow-lg border-2 border-gray-200">
+                    <FiArrowUp size={25} color="red" />
+                    <div>
+                      <div className="text-gray-400 text-lg">
+                        Transfer to Deni
+                      </div>
+                      <div className="font-bold text-xl">Rp220.000</div>
+                    </div>
+                  </div>
+                  <div className="flex gap-3 items-center p-5 rounded-lg shadow-lg border-2 border-gray-200">
+                    <FiArrowUp size={25} color="red" />
+                    <div>
+                      <div className="text-gray-400 text-lg">
+                        Transfer to Jessica Lee
+                      </div>
+                      <div className="font-bold text-xl">Rp220.000</div>
+                    </div>
+                  </div>
+                  <div className="flex gap-3 items-center p-5 rounded-lg shadow-lg border-2 border-gray-200">
+                    <FiArrowDown size={25} color="green" />
+                    <div>
+                      <div className="text-gray-400 text-lg">
+                        Accept from Joshua Lee
+                      </div>
+                      <div className="font-bold text-xl">Rp220.000</div>
+                    </div>
+                  </div>
+                </div>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- pages/transfer.js
+++ pages/transfer.js
@@ -9,6 +9,9 @@ import Link from 'next/link'
 import Picture from '../public/picture.jpg'
 import Image from 'next/image'
 import Head from 'next/head'
+import Header from '@/components/header'
+import Sidebar from '@/components/sidebar'
+import Footer from '@/components/footer'
 
 export default function Transfer() {
   return (
@@ -16,54 +19,9 @@ export default function Transfer() {
       <Head>
         <title>Transfer</title>
       </Head>
-      <div className="flex justify-between items-center w-full h-[140px] rounded-2xl shadow-2xl p-12">
-        <Link href="/" className="text-3xl font-bold text-primary">
-          PayWave
-        </Link>
-        <div className="flex justify-center items-center gap-14">
-          <div className="flex gap-3">
-            <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
-              <Image src={Picture} alt=""></Image>
-            </div>
-            <div className="flex flex-col gap-1">
-              <div className="font-semibold">Robert Chandler</div>
-              <div className="text-gray-600">+62 8139 3877 7946</div>
-            </div>
-          </div>
-          <div>
-            <FiBell size={25} />
-          </div>
-        </div>
-      </div>
+      <Header />
       <div className="flex gap-10 w-full h-[800px] py-10 px-20">
-        <div className="w-[270px] h-full rounded-2xl shadow-2xl ">
-          <div className="flex flex-col justify-between w-full h-full px-8 py-8">
-            <div className="flex justify-center flex-col gap-10">
-              <div className="flex gap-5 items-center text-xl cursor-pointer hover:text-primary">
-                <RxDashboard />
-                Dashboard
-              </div>
-              <div className="flex gap-5 items-center text-xl cursor-pointer hover:text-primary">
-                <FiArrowUp />
-                Transfer
-              </div>
-              <div className="flex gap-5 items-center text-xl cursor-pointer hover:text-primary">
-                <FiPlus />
-                Top Up
-              </div>
-              <div className="flex gap-5 items-center text-xl cursor-pointer hover:text-primary">
-                <BsPersonCircle />
-                Profile
-              </div>
-            </div>
-            <div>
-              <div className="flex gap-5 items-center text-xl cursor-pointer hover:text-red-500">
-                <FiLogOut />
-                Log Out
-              </div>
-            </div>
-          </div>
-        </div>
+        <Sidebar />
         <div className="w-full flex flex-col gap-5">
           <div className="flex flex-col gap-10 w-full rounded-2xl shadow-2xl h-full p-10">
             <div className="text-lg font-bold">Search Receiver</div>
@@ -163,13 +121,7 @@ export default function Transfer() {
           </div>
         </div>
       </div>
-      <div className="flex justify-between items-center w-full h-[68px] bg-primary text-white font-semibold px-10">
-        <div>2020 FazzPay. All right reserved.</div>
-        <div className="flex gap-5">
-          <div>+62 5637 8882 9901</div>
-          <div>contact@fazzpay.com</div>
-        </div>
-      </div>
+      <Footer />
     </>
   )
 }
