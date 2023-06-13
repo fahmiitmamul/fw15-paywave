9b373e2fc4e05561d6f3f4b2dd9e0b952a6347a8
fahmipraga <itmamul2004@gmail.com>
2023-06-14 02:12:37 +0700

    feat: add redux transfer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import profileReducer from './profile'
 import messageReducer from './message'
+import transferReducer from './transfer'
 
 const reducer = combineReducers({
   profile: profileReducer,
   message: messageReducer,
+  transfer: transferReducer,
 })
 
 export default reducer
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-
 import reducer from './reducers'
 
 export const store = configureStore({
