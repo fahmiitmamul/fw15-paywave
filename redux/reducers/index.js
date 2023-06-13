660484f1210b79c3cc83c00991dbe49ec91c6b50
fahmipraga <itmamul2004@gmail.com>
2023-06-13 15:06:24 +0700

    feat: add message reducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import profileReducer from './profile'
+import messageReducer from './message'
 
 const reducer = combineReducers({
   profile: profileReducer,
+  message: messageReducer,
 })
 
 export default reducer
