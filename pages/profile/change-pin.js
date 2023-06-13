d71bcb0d7df2dc521e023571cfbd71b1e967dd18
fahmipraga <itmamul2004@gmail.com>
2023-06-13 22:31:24 +0700

    fix: fix change pin

--- pages/profile/change-pin.js
+++ pages/profile/change-pin.js
@@ -1,14 +1,16 @@
 import Footer from '@/components/footer'
 import Header from '@/components/header'
-import PinInput from '@/components/pin-input'
 import Sidebar from '@/components/sidebar'
 import cookieConfig from '@/helpers/cookie-config'
 import http from '@/helpers/http'
+import * as Yup from 'yup'
+import Lock from '../../public/lock.svg'
+import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import { withIronSessionSsr } from 'iron-session/next'
 import { useDispatch } from 'react-redux'
 import { getProfileAction } from '@/redux/actions/profile'
-import { useRouter } from 'next/router'
+import { Formik } from 'formik'
 
 export const getServerSideProps = withIronSessionSsr(async ({ req }) => {
   const token = req.session.token || null
@@ -20,11 +22,16 @@ export const getServerSideProps = withIronSessionSsr(async ({ req }) => {
 }, cookieConfig)
 
 export default function ChangePin({ token }) {
-  const [pin, setPin] = useState('')
   const [errormessage, seterrormessage] = useState('')
-  const dispatch = useDispatch()
-  const router = useRouter()
+  const [successMsg, setsuccessMsg] = useState('')
   const [loading, setLoading] = useState('')
+  const dispatch = useDispatch()
+
+  const validationSchema = Yup.object({
+    oldPin: Yup.number().required('Old PIN is required !'),
+    newPin: Yup.number().required('New PIN is required !'),
+    confirmPin: Yup.number().required('Confirm PIN is required !'),
+  })
 
   useEffect(() => {
     dispatch(getProfileAction(token))
@@ -36,23 +43,23 @@ export default function ChangePin({ token }) {
     }, 3000)
   }
 
-  async function doSubmit(e) {
+  async function doSubmit(values) {
     e.preventDefault()
     try {
       setLoading(true)
-      const oldPin = pin
-      let newPin
-      let confirmPin
+      const oldPin = values.oldPin
+      const newPin = values.newPin
+      const confirmPin = values.confirmPin
       const form = new URLSearchParams({
         oldPin,
         newPin,
         confirmPin,
       }).toString()
       const { data } = await http(token).patch('/profile/change-pin', form)
-      setLoading(false)
       if (data) {
-        router.push('/profile/change-newpin')
+        setsuccessMsg('PIN Changed successfully')
       }
+      setLoading(false)
     } catch (err) {
       const message = err.response?.data?.message
       if (message === 'profile_change_pin_wrong_old') {
@@ -68,7 +75,7 @@ export default function ChangePin({ token }) {
       <div className="flex gap-10 w-full h-[800px] py-10 px-20">
         <Sidebar />
         <div className="w-full flex flex-col gap-5">
-          <div className="flex flex-col gap-10 w-full rounded-2xl shadow-2xl h-full p-10">
+          <div className="flex flex-col w-full rounded-2xl shadow-2xl h-full p-10">
             <div className="flex flex-col gap-5 w-2/6">
               <div className="font-bold text-2xl">Change Pin</div>
               <div>
@@ -76,28 +83,144 @@ export default function ChangePin({ token }) {
                 next steps.
               </div>
             </div>
-            <div className="flex w-full mx-auto mt-10 flex-col justify-center items-center gap-8 ">
+            <div className="flex w-full mx-auto mt-10 flex-col gap-5">
               {errormessage && (
                 <div className="alert alert-error max-w-md text-lg text-white">
                   {errormessage}
                 </div>
               )}
-              <div className="font-bold text-lg">Enter your current pin</div>
-              <form
+              {successMsg && (
+                <div className="alert alert-success max-w-md text-lg text-white">
+                  {successMsg}
+                </div>
+              )}
+              <Formik
+                initialValues={{
+                  oldPin: '',
+                  newPin: '',
+                  confirmPin: '',
+                }}
+                validationSchema={validationSchema}
                 onSubmit={doSubmit}
-                className="w-full flex flex-col gap-10 items-center justify-center"
               >
-                <PinInput onChangePin={setPin} />
-                <button
-                  type="submit"
-                  className="btn btn-primary w-1/2 normal-case max-w-md text-white"
-                >
-                  {loading && (
-                    <span className="loading loading-spinner loading-sm"></span>
-                  )}
-                  {!loading && 'Change PIN'}
-                </button>
-              </form>
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleBlur,
+                  handleSubmit,
+                  handleChange,
+                  isSubmitting,
+                }) => {
+                  return (
+                    <form
+                      onSubmit={handleSubmit}
+                      className="flex flex-col gap-5"
+                    >
+                      <div className="flex flex-col gap-5">
+                        <div className="flex flex-col gap-8">
+                          <div className="max-w-lg relative">
+                            <input
+                              type="number"
+                              name="oldPin"
+                              id="oldPin"
+                              value={values.oldPin}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={`border-b-2 border-gray-300 focus:border-indigo-500 focus:outline-none w-full transition duration-300 ease-in-out px-8 py-1 ${
+                                touched.oldPin &&
+                                errors.oldPin &&
+                                'border-b-red-500'
+                              }`}
+                              placeholder="Enter Old Pin"
+                            ></input>
+                            <Image
+                              alt=""
+                              src={Lock}
+                              className="absolute top-0"
+                            />
+                            {errors.oldPin && touched.oldPin && (
+                              <label htmlFor="oldPin" className="label">
+                                <span className="label-text-alt text-error">
+                                  {errors.oldPin}
+                                </span>
+                              </label>
+                            )}
+                          </div>
+                          <div className="max-w-lg relative">
+                            <input
+                              type="number"
+                              name="newPin"
+                              id="newPin"
+                              value={values.newPin}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={`border-b-2 border-gray-300 focus:border-indigo-500 focus:outline-none w-full transition duration-300 ease-in-out px-8 py-2 ${
+                                touched.newPin &&
+                                errors.newPin &&
+                                'border-b-red-500'
+                              }`}
+                              placeholder="Enter New Pin"
+                            ></input>
+                            <Image
+                              alt=""
+                              src={Lock}
+                              className="absolute top-2"
+                            />
+                            {errors.newPin && touched.newPin && (
+                              <label htmlFor="newPin" className="label">
+                                <span className="label-text-alt text-error">
+                                  {errors.newPin}
+                                </span>
+                              </label>
+                            )}
+                          </div>
+                          <div className="max-w-lg relative">
+                            <input
+                              type="number"
+                              name="confirmPin"
+                              id="confirmPin"
+                              value={values.confirmPin}
+                              onChange={handleChange}
+                              onBlur={handleBlur}
+                              className={`border-b-2 border-gray-300 focus:border-indigo-500 focus:outline-none w-full transition duration-300 ease-in-out px-8 py-2 ${
+                                touched.confirmPin &&
+                                errors.confirmPin &&
+                                'border-b-red-500'
+                              }`}
+                              placeholder="Confirm PIN"
+                            ></input>
+                            <Image
+                              alt=""
+                              src={Lock}
+                              className="absolute top-2"
+                            />
+                            {errors.confirmPin && touched.confirmPin && (
+                              <label htmlFor="confirmPin" className="label">
+                                <span className="label-text-alt text-error">
+                                  {errors.confirmPin}
+                                </span>
+                              </label>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <button
+                          type="submit"
+                          className="btn btn-primary normal-case max-w-lg w-full text-white shadow-2xl"
+                          disabled={isSubmitting}
+                        >
+                          {loading && (
+                            <span className="loading loading-spinner loading-sm"></span>
+                          )}
+                          {!loading && 'Change Pin'}
+                        </button>
+                      </div>
+                    </form>
+                  )
+                }}
+              </Formik>
             </div>
           </div>
         </div>
