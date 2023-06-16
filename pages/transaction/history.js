4e8f34800b573430f77e02d3344af4467e07809b
fahmipraga <itmamul2004@gmail.com>
2023-06-17 00:53:28 +0700

    fix: history

--- pages/transaction/history.js
+++ pages/transaction/history.js
@@ -1,16 +1,18 @@
-import Picture from '../../public/picture.jpg'
 import Image from 'next/image'
 import Header from '@/components/header'
 import Sidebar from '@/components/sidebar'
 import Footer from '@/components/footer'
 import Head from 'next/head'
+import Default from '../../public/images.png'
 import { useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 import { withIronSessionSsr } from 'iron-session/next'
 import { getProfileAction } from '@/redux/actions/profile'
 import cookieConfig from '@/helpers/cookie-config'
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 import { setMessage } from '@/redux/reducers/message'
+import http from '@/helpers/http'
 
 export const getServerSideProps = withIronSessionSsr(async ({ req }) => {
   const token = req.session.token || null
@@ -24,6 +26,8 @@ export const getServerSideProps = withIronSessionSsr(async ({ req }) => {
 export default function History({ token }) {
   const dispatch = useDispatch()
   const router = useRouter()
+  const [transactions, setTransaction] = useState([])
+  const [errorFetch, setErrorFetch] = useState('')
 
   useEffect(() => {
     dispatch(getProfileAction(token))
@@ -31,6 +35,20 @@ export default function History({ token }) {
       router.push('/auth/login')
       dispatch(setMessage('You have to login first !'))
     }
+
+    async function fetchHistoryTransaction() {
+      try {
+        const { data } = await http(token).get('/transactions?page=1&limit=5')
+        setTransaction(data.results)
+      } catch (err) {
+        const message = err.response?.data?.message
+        if (message) {
+          setErrorFetch('Message')
+        }
+      }
+    }
+
+    fetchHistoryTransaction()
   }, [dispatch, token, router])
 
   return (
@@ -65,53 +83,86 @@ export default function History({ token }) {
               </div>
             </div>
             <div className="flex flex-col gap-10 w-full">
-              <div className="flex items-center w-full justify-between">
-                <div className="flex gap-5">
-                  <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
-                    <Image src={Picture} alt=""></Image>
+              <div className="flex flex-col gap-10 w-full overflow-scroll">
+                {transactions.map((item) => (
+                  <div
+                    key={item.id}
+                    className="flex items-center w-full justify-between"
+                  >
+                    <div className="flex w-full gap-5">
+                      {item.type === 'TOP-UP' && (
+                        <>
+                          <div className="flex w-full justify-between">
+                            <div className="flex gap-5">
+                              <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
+                                {!item.recipient.picture && (
+                                  <div>
+                                    <Image src={Default} alt=""></Image>
+                                  </div>
+                                )}
+                                {item.recipient.picture && (
+                                  <Image
+                                    src={item.recipient.picture}
+                                    alt=""
+                                    width={60}
+                                    height={60}
+                                  ></Image>
+                                )}
+                              </div>
+                              <div>
+                                <div className="font-semibold">
+                                  {item.recipient.fullName ||
+                                    item.recipient.email}
+                                </div>
+                                <div className="text-[#7A7886]">
+                                  {item.type}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="font-bold text-green-600">
+                              + Rp{Number(item.amount).toLocaleString('id')}
+                            </div>
+                          </div>
+                        </>
+                      )}
+                      {item.type === 'TRANSFER' && (
+                        <>
+                          <div className="flex w-full justify-between">
+                            <div className="flex gap-5">
+                              <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
+                                {!item.recipient.picture && (
+                                  <div>
+                                    <Image src={Default} alt=""></Image>
+                                  </div>
+                                )}
+                                {item.recipient.picture && (
+                                  <Image
+                                    src={item.recipient.picture}
+                                    alt=""
+                                    width={60}
+                                    height={60}
+                                  ></Image>
+                                )}
+                              </div>
+                              <div>
+                                <div className="font-semibold">
+                                  {item.recipient.fullName ||
+                                    item.recipient.email}
+                                </div>
+                                <div className="text-[#7A7886]">
+                                  {item.type}
+                                </div>
+                              </div>
+                            </div>
+                            <div className="font-bold text-red-600">
+                              - Rp{Number(item.amount).toLocaleString('id')}
+                            </div>
+                          </div>
+                        </>
+                      )}
+                    </div>
                   </div>
-                  <div>
-                    <div className="font-semibold">Samuel Suhi</div>
-                    <div className="text-[#7A7886]">Accept</div>
-                  </div>
-                </div>
-                <div className="font-bold text-green-600">+Rp50.000</div>
-              </div>
-              <div className="flex items-center w-full justify-between">
-                <div className="flex gap-5">
-                  <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
-                    <Image src={Picture} alt=""></Image>
-                  </div>
-                  <div>
-                    <div className="font-semibold">Netflix</div>
-                    <div className="text-[#7A7886]">Transfer</div>
-                  </div>
-                </div>
-                <div className="font-bold text-red-600">+Rp50.000</div>
-              </div>
-              <div className="flex items-center w-full justify-between">
-                <div className="flex gap-5">
-                  <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
-                    <Image src={Picture} alt=""></Image>
-                  </div>
-                  <div>
-                    <div className="font-semibold">Christine</div>
-                    <div className="text-[#7A7886]">Accept</div>
-                  </div>
-                </div>
-                <div className="font-bold text-green-600">+Rp50.000</div>
-              </div>
-              <div className="flex items-center w-full justify-between">
-                <div className="flex gap-5">
-                  <div className="w-[52px] h-[52px] rounded-lg overflow-hidden">
-                    <Image src={Picture} alt=""></Image>
-                  </div>
-                  <div>
-                    <div className="font-semibold">Robert Chandler</div>
-                    <div className="text-[#7A7886]">Topup</div>
-                  </div>
-                </div>
-                <div className="font-bold text-red-600">+Rp50.000</div>
+                ))}
               </div>
             </div>
           </div>
