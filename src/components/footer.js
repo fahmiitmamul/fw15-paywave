be3df727a7e428e429272b0f0489b3a0d2a24707
fahmipraga <itmamul2004@gmail.com>
2023-08-02 00:30:34 +0700

    fix: app name

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,10 +2,10 @@ export default function Footer() {
   return (
     <>
       <div className="flex flex-wrap justify-between items-center w-full xl:h-[68px] bg-primary text-white font-semibold px-10 py-10">
-        <div>2020 FazzPay. All right reserved.</div>
+        <div>2023 PayWave. All right reserved.</div>
         <div className="flex flex-wrap gap-5">
           <div>+62 5637 8882 9901</div>
-          <div>contact@fazzpay.com</div>
+          <div>contact@paywave.com</div>
         </div>
       </div>
     </>
